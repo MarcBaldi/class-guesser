6890d328eacff18b6f0358b7aa7ea40e90570096
Marcimio <marc.baldi@hotmail.de>
2025-07-13 02:42:29 +0200

    smoothe fixes

--- pages/classguess.vue
+++ pages/classguess.vue
@@ -17,6 +17,18 @@ const classes = [
   'Elementalist',
 ]
 
+const classesBg = [
+  '#72C1D9',
+  '#FFD166',
+  '#D16E5A',
+  '#D09C59',
+  '#C08F95',
+  '#8CDC82',
+  '#B679D5',
+  '#52A76F',
+  '#F68A87',
+]
+
 onMounted(async () => {
   if (!canvasRef.value) return
 
@@ -49,11 +61,13 @@ onMounted(async () => {
       fontWeight: 'bold',
       originX: 'center',
       selectable: false,
+      backgroundColor: classesBg[i]
     })
     canvas.add(text)
 
   }
-
+  let hObj = [null, null, null, null, null, null, null, null, null]
+  let imgPositions = new Array(20)
   // Bild laden via Promise API
   for (let i = 0; i <= 8; i++) {
 
@@ -66,10 +80,14 @@ onMounted(async () => {
     img.left = leftImg
     img.top = topImg
     img.selectable = true
-    img.hasControls = true
+    img.hasControls = false
 
     canvas.add(img)
     canvas.setActiveObject(img)
+
+    hObj[i] = img
+    //coords[i] = { left: leftImg, top: topCoords }
+
   }
 
   // Snap-Funktion
@@ -77,16 +95,40 @@ onMounted(async () => {
   const leftOffset = 25;
   const topOffset = 50;
   function snapToGrid(obj) {
-
+    if (hObj == obj) {
+      console.log("uhoh")
+    }
+    const snapLeft = Math.min(Math.max(Math.round((obj.left - leftOffset) / gridSize) * gridSize + leftOffset, leftOffset), 625)
+    const snapTop = Math.min(Math.max(Math.round((obj.top - topOffset) / gridSize) * gridSize + topOffset, topOffset), 500)
     obj.set({
-      left: Math.min(Math.max(Math.round((obj.left - leftOffset) / gridSize) * gridSize + leftOffset, leftOffset), 625),
-      top:  Math.min(Math.max(Math.round((obj.top - topOffset) / gridSize) * gridSize + topOffset, topOffset), 500)
+      left: snapLeft,
+      top: snapTop
     });
     obj.setCoords(); // ganz entscheidend!
+
+    const zielPos = ((snapTop - 50)  / 150 * 5 + (snapLeft - 25) / 150)
+    imgPositions[zielPos] = obj
+
+    //console.log(zielPos)
+
+    /*hObj.forEach((h)=>{
+      if (h.left == obj.left && h.top == obj.top) {
+        console.log(h)
+        console.log(obj)
+
+        h.set({
+          left: 0,
+          top: 0
+        });
+      }
+    })*/
+
+
+
   }
 
   // Wähle, wann gesnappt wird:
-  canvas.on('object:moving', e => {
+  canvas.on('object:modified', e => {
     snapToGrid(e.target);
   });
 })
@@ -96,15 +138,39 @@ onBeforeUnmount(() => {
   canvas?.dispose()
 })
 
+const copyToClip = async function () {
+  try {
+    // Canvas in Blob konvertieren
+    const blob = await new Promise(resolve =>
+        canvas.toCanvasElement().toBlob(resolve, 'image/png')
+    );
+
+    // In Zwischenablage schreiben (ClipboardItem)
+    await navigator.clipboard.write([
+      new ClipboardItem({ 'image/png': blob })
+    ]);
+
+    alert('Bild wurde in die Zwischenablage kopiert!');
+  } catch (err) {
+    console.error('Fehler beim Kopieren in die Zwischenablage:', err);
+    alert('Kopieren nicht möglich. Browser unterstützt evtl. keine Bild-Zwischenablage.');
+  }
+}
+
 </script>
 
 <template>
   <h2 class="text-center">Class guesser</h2>
   <p class="text-center">
+    <span>Event: </span>
     <a href="https://bsky.app/profile/guildwars2.com">https://bsky.app/profile/guildwars2.com</a>
   </p>
-  <div class="d-flex canvas-container">
+  <div class="d-flex canvas-container justify-center">
     <canvas ref="canvasRef" width="800" height="600" style="border:1px solid #ccc;"></canvas>
+  </div>
+  <div class="d-flex justify-center">
+    <v-btn title="Coming soon" disabled>Save</v-btn>
+    <v-btn @click="copyToClip">Copy</v-btn>
   </div>
   <p><small>NCSOFT, the interlocking NC logo, ArenaNet, Guild Wars, Guild Wars Factions, Guild Wars Nightfall, Guild Wars: Eye of the North, Guild Wars 2, and all associated logos and designs are trademarks or registered trademarks of NCSOFT Corporation. All other trademarks are the property of their respective owners.
   </small></p>
@@ -113,9 +179,5 @@ onBeforeUnmount(() => {
 <style scoped>
 canvas {
   border: 1px solid #ccc;
-}
-
-.canvas-container {
-  justify-content: center;
 }
 </style>
